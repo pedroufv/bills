33f7f2616d1a0102d70e4636e2e12f2a0a4769ba
Pedro Augusto Mendes e Silva <pedro.e@ufv.br>
2019-09-25 10:44:33 -0300

    test if can get user by token

---
+++ tests/Feature/ApiTokenTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class ApiTokenTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::create([
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => $this->faker->password,
+            'api_token' => Str::random(60)
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function canGetUserByToken()
+    {
+        $response = $this->json('GET', '/api/user', [
+            'api_token' => $this->user->api_token
+        ]);
+
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'name' => $this->user->name,
+                'email' => $this->user->email,
+            ]);
+    }
+}
